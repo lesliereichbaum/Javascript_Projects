6499edccd247a7baec0c522dd8ba3e4c4644f1ad
Leslie <lreichbaum@gmail.com>
2020-06-29 17:40:05 -0400

    pracitcing using function and operator

---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,10 @@
+function changeColor(newColor) {
+    var elem = document.getElementById('para');
+    elem.style.color = newColor;
+  }
+
+  function myFunction() {
+      var sentence="I am learning";
+      sentence += "a lot from this course!";
+      document.getElementById("Concatenate").innerHTML = sentence;
+  }
