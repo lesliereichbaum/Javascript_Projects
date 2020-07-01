7a9bb217010aaf5e173c089976a20c489b6959f0
Leslie <lreichbaum@gmail.com>
2020-07-01 18:14:52 -0400

    Javascript 'Are you Old Enough to Vote' Program

--- JS/Voting_Javascript.js
+++ JS/Voting_Javascript.js
@@ -1,6 +1,6 @@
 function Vote_Age() {
     var Age, Can_vote;
-    Age = document.getElementById("votingAge").value;
+    Age = document.getElementById("Age").value;
     Can_vote = (Age<18) ? "You are too young" : "You are old enough";
     document.getElementById("votingAge").innerHTML = Can_vote + "to vote.";
 
