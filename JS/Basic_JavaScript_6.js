555c9197cd612cd5a82c8d58274d3a6a683a7760
Leslie <lreichbaum@gmail.com>
2020-07-01 17:26:52 -0400

    Nested Function

--- JS/Basic_JavaScript_6.js
+++ JS/Basic_JavaScript_6.js
@@ -1,16 +1,9 @@
-
-function School(name, location, founded, mascot) {
-    this.School_name= name;
-    this.School_location= location;
-    this.School_founded= founded;
-    this.School_mascot = mascot;
-}
-
-var Alyssa = new School ("Harvard", "Cambridge", 1650, "No_Mascot");
-var Anna = new School("Ohio State", "Columbus", 1870, "Brutus");
-var Sonny = new School("USC", "Los Angeles", 1850, "Trojan");
-function myFunction() {
-    document.getElementById("New_and_This").innerHTML=
-    "Alssay goes to " + Alyssa.School_name + "in" + Alyssa.School_location +
-    "which was founded in" + Alyssa.School_founded;
+function count_Function () {
+    document.getElementById("Nested_Function").innerHTML=Count();
+    function Count () {
+        var Starting_point = 22;
+        function Plus_one() {Starting_point += 4;}
+        Plus_one();
+        return Starting_point;
+    }
 }
