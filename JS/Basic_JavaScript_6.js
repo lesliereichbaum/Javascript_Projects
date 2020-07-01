94e3a51511dcb4260104b3fd7f68e90f779acb37
Leslie <lreichbaum@gmail.com>
2020-07-01 16:08:45 -0400

    Using New and This keywords

--- JS/Basic_JavaScript_6.js
+++ JS/Basic_JavaScript_6.js
@@ -1,7 +1,16 @@
-function Ride_Function() {
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_ride = (Height<52) ? "You are too short" : "You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + "to ride.";
 
+function School(name, location, founded, mascot) {
+    this.School_name= name;
+    this.School_location= location;
+    this.School_founded= founded;
+    this.School_mascot = mascot;
+}
+
+var Alyssa = new School ("Harvard", "Cambridge", 1650, "No_Mascot");
+var Anna = new School("Ohio State", "Columbus", 1870, "Brutus");
+var Sonny = new School("USC", "Los Angeles", 1850, "Trojan");
+function myFunction() {
+    document.getElementById("New_and_This").innerHTML=
+    "Alssay goes to " + Alyssa.School_name + "in" + Alyssa.School_location +
+    "which was founded in" + Alyssa.School_founded;
 }
