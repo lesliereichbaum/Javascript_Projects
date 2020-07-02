e4d607dc49f9c9b58e0fa33f7b713b4f405bf3d7
Leslie <lreichbaum@gmail.com>
2020-07-02 11:40:46 -0400

    search, uppercase and slice method

--- JS/Basic_JavaScript_8.js
+++ JS/Basic_JavaScript_8.js
@@ -1,3 +1,5 @@
+
+//CONCATENATED STRING 
 function CC_String() {
     var part_1 = "Twinkle twinkle ";
     var part_2 = "little star";
@@ -6,4 +8,28 @@ function CC_String() {
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
     document.getElementById("Con_Cat").innerHTML = whole_sentence;
     
+}
+
+//Slice() METHOD 
+
+function slice_Method () {
+    var Sentence = "Come play with us Danny";
+    var Section = Sentence.slice (9, 14);
+    document.getElementById("Slice").innerHTML=Section;
+}
+
+//UPPER CASE METHOD 
+
+function myUpperCase() {
+    var text= document.getElementById("uppercase").innerHTML;
+    document.getElementById("uppercase").innerHTML = text.toUpperCase();
+
+}
+
+//Search Method 
+
+function searching() {
+    var str = "Please find where locate occurs!. Just kidding, find 'where'!";
+    var pos = str.search("where");
+document.getElementById("search").innerHTML = pos;
 }
